8210dec25bee9d9bd4f17554c3b9d86162370ceb
Pas-olo <62897181+Pas-olo@users.noreply.github.com>
2022-06-27 10:47:42 +0200

    r

--- js/incrnub.js
+++ js/incrnub.js
@@ -2,23 +2,30 @@ const counters = document.querySelector('.value');
 const speed = 200;
 
 
-function counnnter(){
-	counters.forEach( counter => {
-	   const animate = () => {
-		  const value = +counter.getAttribute('akhi');
-		  const data = +counter.innerText;
-		 
-		  const time = value / speed;
-		 if(data < value) {
-			  counter.innerText = Math.ceil(data + time);
-			  setTimeout(animate, 1);
-			}else{
-			  counter.innerText = value;
-			}
-		 
-	   }
-	   
-	});
+
+counters.forEach( counter => {
+   const animate = () => {
+	  const value = +counter.getAttribute('akhi');
+	  const data = +counter.innerText;
+	 
+	  const time = value / speed;
+	 if(data < value) {
+		  counter.innerText = Math.ceil(data + time);
+		  setTimeout(animate, 1);
+		}else{
+		  counter.innerText = value;
+		}
+	 
+   }
+   
+});
+
+function isOnScreen(element)
+{
+    var curPos = element.offset();
+    var curTop = curPos.top;
+    var screenHeight = $(window).height();
+    return (curTop > screenHeight) ? false : true;
 }
 
-counters.onclick = animate();
+if(isOnScreen(counters)) {animate()};
